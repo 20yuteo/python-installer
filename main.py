85c3464581fef76e5b60955a535298ea40300ea0
20yuteo <20yuteo@gmail.com>
2023-06-09 14:02:04 +0000

    requeirementsを追加

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from yt_dlp import YoutubeDL
 
 import sys
+import glob
+import os
+import subprocess
 
 args = sys.argv
 link = args[1]
 
 ydl = YoutubeDL()
-result = ydl.download([link])
+ydl.download([link])
+
+videos = glob.glob('./*.mp4')
+
+for video in videos:
+    os.rename(video, video.replace(' ', ''))
+    
+videos = glob.glob('./*.mp4')
+
+count = 0
+
+for video in videos:
+    size = os.path.getsize(video)
+    if size>1000000000:
+
+        subprocess.call('ffmpeg -i '+video+' -crf 18 '+video[:-3]+'_圧縮済.mp4', shell=True)
+
+        os.rename(video, './'+str(count)+'_圧縮前_'+video[2:])
+        count += 1
