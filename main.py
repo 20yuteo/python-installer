5a797579870b5b604fe78f2d5ac504fa7d2fed14
20yuteo <20yuteo@gmail.com>
2023-06-10 14:16:28 +0000

    各処理を関数化

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ import glob
 import os
 import subprocess
 
+def download(link):
+    ydl = YoutubeDL()
+    ydl.download([link])
+
+def compress():
+    videos = glob.glob('./*.mp4')
+
+    for video in videos:
+        os.rename(video, video.replace(' ', ''))
+        
+    videos = glob.glob('./*.mp4')
+
+    count = 0
+
+    for video in videos:
+        size = os.path.getsize(video)
+        if size>1000000000:
+
+            subprocess.call('ffmpeg -i '+video+' -crf 18 '+video[:-3]+'_圧縮済.mp4', shell=True)
+
+            os.rename(video, './'+str(count)+'_圧縮前_'+video[2:])
+            count += 1
+
 args = sys.argv
 link = args[1]
 
-ydl = YoutubeDL()
-ydl.download([link])
-
-videos = glob.glob('./*.mp4')
-
-for video in videos:
-    os.rename(video, video.replace(' ', ''))
-    
-videos = glob.glob('./*.mp4')
-
-count = 0
-
-for video in videos:
-    size = os.path.getsize(video)
-    if size>1000000000:
-
-        subprocess.call('ffmpeg -i '+video+' -crf 18 '+video[:-3]+'_圧縮済.mp4', shell=True)
-
-        os.rename(video, './'+str(count)+'_圧縮前_'+video[2:])
-        count += 1
+download(link)
+compress()
